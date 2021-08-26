9c0f2ecea5e64d635a3acbb909a6b949dfd64ffe
Andreas Schneider <parklez@live.com>
2021-08-25 21:38:31 -0300

    Add "Post" component to new ":id" Projects routing

--- my-bloog/src/app/projects/projects-routing.module.ts
+++ my-bloog/src/app/projects/projects-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsHomeComponent } from './projects-home/projects-home.component';
+import { PostComponent } from '../shared/post/post.component';
 
 const routes: Routes = [
   {
     path: '', component: ProjectsHomeComponent
+  },
+  {
+    path: ':id', component: PostComponent
   }
 ];
 
