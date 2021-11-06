66aba80c428b482fd2695d4afcd185df04571bd3
Andreas <parklez@live.com>
2021-11-06 12:30:43 -0300

    Add port config to express

---
+++ server/src/config/app.js
@@ -0,0 +1,3 @@
+const port = process.env.PORT || 8080;
+
+module.exports = port;
--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = require('./config/app');
+
 app.use(express.static(`${__dirname}/../public`));
 
 // https://www.devmedia.com.br/subindo-uma-aplicacao-angular-para-o-heroku/40260
@@ -10,4 +12,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../public/index.html`));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
