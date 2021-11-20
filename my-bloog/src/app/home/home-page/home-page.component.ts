02b64e516946405b10c070fd84bc8efe5acc7c72
Andreas <parklez@live.com>
2021-11-20 12:39:13 -0300

    Fetch Posts in home-page!

--- my-bloog/src/app/home/home-page/home-page.component.ts
+++ my-bloog/src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../../shared/services/posts.service'
+import { Post } from '../../shared/models/post';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  retrievedPosts: Post[] = [];
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.postsService.getSomePosts().subscribe((posts: Post[]) => {
+      this.retrievedPosts = posts;
+    })
   }
 
 }
