1c2146128980c10a86b54fe1b27c5957223cf074
Andreas <parklez@live.com>
2021-11-20 16:42:44 -0300

    Add app-toasty-notification & use toasty in HomePage

--- my-bloog/src/app/home/home-page/home-page.component.ts
+++ my-bloog/src/app/home/home-page/home-page.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { PostsService } from '../../shared/services/posts.service'
+import { PostsService } from '../../shared/services/posts.service';
 import { Post } from '../../shared/models/post';
+import { ToastyService } from 'src/app/shared/services/toasty.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
-
   retrievedPosts: Post[] = [];
 
-  constructor(private postsService: PostsService) { }
+  constructor(
+    private postsService: PostsService,
+    private toast: ToastyService
+  ) {}
 
   ngOnInit(): void {
-    this.postsService.getSomePosts().subscribe((posts: Post[]) => {
-      this.retrievedPosts = posts;
-    })
+    this.postsService.getSomePosts().subscribe(
+      (posts: Post[]) => {
+        this.retrievedPosts = posts;
+      },
+      (error) => {
+        console.log(error); // Create toasty!
+        this.toast.pushNewToasty(
+          'Oops! - Looks like panda couldnt get any posts -- ' + error.message,
+          'danger'
+        );
+      }
+    );
   }
-
 }
