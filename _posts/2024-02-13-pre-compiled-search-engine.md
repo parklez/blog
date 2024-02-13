9df1dbcca6462386a5c201e2f16d72ba025b5b82
Andreas <>
2024-02-13 20:29:46 -0300

    Improve content compiler, Add post

---
+++ _posts/2024-02-13-pre-compiled-search-engine.md
@@ -0,0 +1,9 @@
+---
+title: AI / My notes are well titled, now It's time to search through them!
+---
+I just recently used [Codeium](https://codeium.com/) to fix my curiosity of "AI" to assist me in my personal coding sessions, and well, it does help with some optimization or suggesting better names but if I didn't know any better it wouldn't be able to write something decent out of it. Overall a fun/productive thing I'll have around for now.
+
+After writing down a simple script to save pre-compiled json pages, I now want to find relevant text within my notes. Aka I need a search engine of sorts. I won't really expand too much into it, I might write my own rudimentary one but for now I'll leave this note with some references. Bye! \
+![worryCoffee](https://cdn.frankerfacez.com/emoticon/350657/2)
+
+- https://www.codementor.io/@rudolfolah/node-js-search-engine-github-s2gh0sfl8
--- blog/compile-static-content.js
+++ blog/compile-static-content.js
@@ -2,136 +2,71 @@ const fs = require("fs");
 const path = require("path");
 const fm = require("front-matter");
 
-const postsDir = path.join(__dirname, "/../_posts/");
-const projectsDir = path.join(__dirname, "/../_projects/");
+const postsDirectory = path.join(__dirname, "../_posts/");
+const projectsDirectory = path.join(__dirname, "../_projects/");
 
-function readFileName(filename) {
+function parseFileName(filename) {
   // https://github.com/jekyll/jekyll/blob/master/lib/jekyll/document.rb#L15C29-L15C86
-  const re = /(\d{2,4}-\d{2}-\d{2})-(.*)/;
-  const name = filename.match(re);
-  const date = name[1] ? name[1] : new Date().toString();
-  return [date, name[2]];
+  const fileNameRegex = /(\d{2,4}-\d{2}-\d{2})-(.*)/;
+  const matchResult = filename.match(fileNameRegex);
+  const date = matchResult[1] ? matchResult[1] : new Date().toString();
+  return [date, matchResult[2]];
 }
 
 function readPost(filename) {
-  const file = fs.readFileSync(path.join(postsDir, filename), "utf8");
-  const content = fm(file);
-  const [date, title] = readFileName(filename);
+  const fileContent = fs.readFileSync(path.join(postsDirectory, filename), "utf8");
+  const frontMatter = fm(fileContent);
+  const [published, postTitle] = parseFileName(filename);
 
   return {
     _id: filename,
     userId: 0,
-    title: content.attributes.title
-      ? content.attributes.title
-      : title.slice(0, -3).replaceAll("-", " "),
-    content: content.body,
+    title: frontMatter.attributes.title || postTitle.slice(0, -3).replaceAll("-", " "),
+    content: frontMatter.body,
     hidden: false,
-    published: date,
+    published: published,
   };
 }
 
 function readProject(filename) {
-  const file = fs.readFileSync(path.join(projectsDir, filename), "utf8");
-  const content = fm(file);
-  const [date, title] = readFileName(filename);
+  const fileContent = fs.readFileSync(path.join(projectsDirectory, filename), "utf8");
+  const frontMatter = fm(fileContent);
+  const [published, title] = parseFileName(filename);
 
   return {
     _id: filename,
-    category: content.attributes.category
-      ? content.attributes.category
-      : 'Other',
-    title: content.attributes.title
-      ? content.attributes.title
-      : title.slice(0, -3).replaceAll("-", " "),
-    thumbnail: content.attributes.thumbnail
-      ? content.attributes.thumbnail
-      : 'assets/640x360.png',
-    link: content.attributes.link
-      ? content.attributes.link
-      : '',
-    description: content.body,
+    category: frontMatter.attributes.category || "Other",
+    title: frontMatter.attributes.title || title.slice(0, -3).replaceAll("-", " "),
+    thumbnail: frontMatter.attributes.thumbnail || "assets/640x360.png",
+    link: frontMatter.attributes.link || "",
+    description: frontMatter.body,
     hidden: false,
-    published: date,
+    published: published,
   };
 }
 
-function compilePosts() {
-  const POSTS = [];
+function compilePages(fileNamePrefix, contentDir, readFunction, itemsPerPage = 10) {
+  const content = fs
+    .readdirSync(contentDir)
+    .map((filename) => readFunction(filename))
+    .sort((a, b) => new Date(b.published) - new Date(a.published));
 
-  // 1. Read file and its content
-  const filesAvailable = fs.readdirSync(postsDir);
+  const paginatedContent = [];
+  for (let i = 0; i < content.length; i += itemsPerPage) {
+    paginatedContent.push(content.slice(i, i + itemsPerPage));
+  }
 
-  filesAvailable.forEach((filename) => {
-    POSTS.push(readPost(filename));
-  });
-
-  // 2. Sort by post.published
-  POSTS.sort(function (a, b) {
-    return new Date(b.published) - new Date(a.published);
-  });
-
-  // 3. Generate a list of posts in expected format
-  const postsPerPage = 10;
-  const total = POSTS.length;
-
-  const splicedPosts = [];
-
-  for (let i = 0; i < POSTS.length; i += postsPerPage)
-    splicedPosts.push(POSTS.slice(i, i + postsPerPage));
-
-  let page = 0;
-
-  splicedPosts.forEach((chunk) => {
+  paginatedContent.forEach((chunk, index) => {
     fs.writeFileSync(
-      path.join(__dirname, `/src/assets/posts-${page}.json`),
+      path.join(__dirname, `/src/assets/${fileNamePrefix}-${index}.json`),
       JSON.stringify({
         results: chunk,
-        total,
-        page,
+        total: content.length,
+        page: index,
       })
     );
-    page += 1;
   });
 }
 
-function compileProjects() {
-  const PROJECTS = [];
-
-  // 1. Read file and its content
-  const filesAvailable = fs.readdirSync(projectsDir);
-
-  filesAvailable.forEach((filename) => {
-    PROJECTS.push(readProject(filename));
-  });
-
-  // 2. Sort by post.published
-  PROJECTS.sort(function (a, b) {
-    return new Date(b.published) - new Date(a.published);
-  });
-
-  // 3. Generate a list of posts in expected format
-  const projectsPerPage = 10;
-  const total = PROJECTS.length;
-
-  const splicedProjects = [];
-
-  for (let i = 0; i < PROJECTS.length; i += projectsPerPage)
-    splicedProjects.push(PROJECTS.slice(i, i + projectsPerPage));
-
-  let page = 0;
-
-  splicedProjects.forEach((chunk) => {
-    fs.writeFileSync(
-      path.join(__dirname, `/src/assets/projects-${page}.json`),
-      JSON.stringify({
-        results: chunk,
-        total,
-        page,
-      })
-    );
-    page += 1;
-  });
-}
-
-compilePosts();
-compileProjects();
+compilePages("projects", projectsDirectory, readProject);
+compilePages("posts", postsDirectory, readPost);
