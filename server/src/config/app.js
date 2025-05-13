034239d9b8316e4221b8765fdec2a5f143ff77dd
Andreas <>
2025-05-13 00:51:11 -0300

    Remove server-side features.
    I made an extra copy to the "server" branch.

--- server/src/config/app.js
+++
@@ -1,4 +0,0 @@
-const port = process.env.PORT || 8080;
-const jwtSecret = process.env.JWT_SECRET || 'YOUWOULDNEVERHAVEGUESSEDITHUH';
-
-module.exports = {port, jwtSecret};
--- server/src/routes/register/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const post = require('./post');
-
-router.post('/', post);
-
-module.exports = router;
